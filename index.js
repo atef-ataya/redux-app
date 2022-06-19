2092db74adefabad991c4e3037bf21badf914a6a
Atef.Hadi.Ataya <77441506+atef-ataya@users.noreply.github.com>
2022-06-19 09:30:17 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,104 @@
+// Library code
+function createStore(reducer) {
+  let state;
+  let listeners = [];
+
+  const getState = () => {
+    return state;
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  };
+
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  };
+}
+
+// App Code
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.todo]);
+    case 'REMOVE_TODO':
+      return state.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE_TODO':
+      return state.map((todo) =>
+        todo.id !== action.id
+          ? todo
+          : Object.assign({}, todo, { complete: !todo.complete })
+      );
+    default:
+      return state;
+  }
+}
+
+function goals(state = [], action) {
+  switch (action.type) {
+    case 'ADD_GOAL':
+      return state.concat([action.goal]);
+    case 'REMOVAL_GOAL':
+      return state.filter((goal) => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  };
+}
+
+const store = createStore(app);
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState());
+});
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false,
+  },
+});
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 1,
+    name: 'Learn Redux1',
+    complete: false,
+  },
+});
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 2,
+    name: 'Learn Redux2',
+    complete: false,
+  },
+});
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 3,
+    name: 'Learn Redux3',
+    complete: false,
+  },
+});
